5ff205447579e2375b942621357143fa327913ab
Lokesh Basu <lokesh.basu@gmail.com>
2021-03-13 15:55:42 +0530

    fix: Update Auditable columns

--- src/main/kotlin/com/dukaankhata/server/entities/Auditable.kt
+++ src/main/kotlin/com/dukaankhata/server/entities/Auditable.kt
@@ -1,13 +1,26 @@
 package com.dukaankhata.server.entities
 
+import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
+import javax.persistence.Column
 
 open class Auditable {
     @CreatedDate
+    @Column(name = "created_at")
     var createdAt: LocalDateTime? = null
 
+    @CreatedBy
+    @Column(name = "created_by")
+    var createdBy: String? = null
+
     @LastModifiedDate
+    @Column(name = "last_modified_at")
     var lastModifiedAt: LocalDateTime? = null
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    var lastModifiedBy: String? = null
 }
