844c28fe5f079412ba62bdb23f0f325799cc98eb
Lokesh Basu <lokesh.basu@gmail.com>
2021-05-24 16:42:42 +0530

    fix: Add auth for swagger (#53)

---
+++ src/main/kotlin/com/dukaankhata/server/config/SwaggerConfiguration.kt
@@ -0,0 +1,57 @@
+package com.dukaankhata.server.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.*
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
+import springfox.documentation.spring.web.plugins.Docket
+import java.util.*
+
+@Configuration
+class SwaggerConfiguration {
+
+    val AUTHORIZATION_HEADER = "Authorization"
+
+    private fun apiInfo(): ApiInfo? {
+        return ApiInfo("DukaanKhata",
+            "DukaanKhata APIs that power mobile, web and workers.",
+            "1.0",
+            "Terms of service",
+            Contact("DukaanKhata Engineering", "www.dukaankhata.com", "salloszraj@gmail.com"),
+            "License of API",
+            "www.dukaankhata.com",
+            Collections.emptyList())
+    }
+
+    @Bean
+    fun api(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
+
+    private fun apiKey(): ApiKey? {
+        return ApiKey("JWT", AUTHORIZATION_HEADER, "header")
+    }
+
+    private fun securityContext(): SecurityContext? {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build()
+    }
+
+    fun defaultAuth(): List<SecurityReference?>? {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes: Array<AuthorizationScope?> = arrayOfNulls<AuthorizationScope>(1)
+        authorizationScopes[0] = authorizationScope
+        return Arrays.asList(SecurityReference("JWT", authorizationScopes))
+    }
+}
