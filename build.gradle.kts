9f94871477f2ee81d427eb164ac40cdfe8bddeb6
Lokesh Basu <lokesh.basu@gmail.com>
2021-10-15 17:07:57 +0530

    fix: Remove git plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id ("com.gorylenko.gradle-git-properties") version "2.2.2"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
 }
