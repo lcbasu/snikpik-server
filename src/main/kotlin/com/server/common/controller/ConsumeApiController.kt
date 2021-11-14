76d2af5a2e568c394f1f2bb9804ad9128cf4edaa
Lokesh Basu <lokesh.basu@gmail.com>
2021-11-14 08:31:32 +0530

    fix: Read request

--- src/main/kotlin/com/server/common/controller/ConsumeApiController.kt
+++ src/main/kotlin/com/server/common/controller/ConsumeApiController.kt
@@ -2,12 +2,22 @@ package com.server.common.controller
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.server.common.service.MediaHandlerService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+
 
 @RestController
 @RequestMapping("consumeAPI")
@@ -20,14 +30,53 @@ class ConsumeApiController {
 
     @RequestMapping(value = ["/awsVideoProcessingCompleted"], method = [RequestMethod.POST])
     @ResponseBody
-    fun awsVideoProcessingCompleted(@RequestBody request: ProcessedVideoSNSRequest, response: HttpServletResponse){
-        logger.info("Call mediaHandlerService for InputFile: ${request.Message?.InputFile}")
-        mediaHandlerService.startProcessingAfterVideoProcessing(request.Message)
+    fun awsVideoProcessingCompleted(request: HttpServletRequest, response: HttpServletResponse){
+        val body = getBody(request)
+        logger.info("body: $body")
+        val bodyObject = try {
+            jacksonObjectMapper().readValue(body, ProcessedVideoSNSRequestBody::class.java)
+        } catch (e: Exception) {
+            null
+        }
+        logger.info("Call mediaHandlerService for InputFile: ${bodyObject?.Message?.InputFile}")
+        mediaHandlerService.startProcessingAfterVideoProcessing(bodyObject?.Message)
+    }
+
+    @Throws(IOException::class)
+    fun getBody(request: HttpServletRequest): String? {
+        var body: String? = null
+        val stringBuilder = StringBuilder()
+        var bufferedReader: BufferedReader? = null
+        try {
+            val inputStream: InputStream? = request.inputStream
+            if (inputStream != null) {
+                bufferedReader = BufferedReader(InputStreamReader(inputStream))
+                val charBuffer = CharArray(128)
+                var bytesRead = -1
+                while (bufferedReader.read(charBuffer).also { bytesRead = it } > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead)
+                }
+            } else {
+                stringBuilder.append("")
+            }
+        } catch (ex: IOException) {
+            throw ex
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close()
+                } catch (ex: IOException) {
+                    throw ex
+                }
+            }
+        }
+        body = stringBuilder.toString()
+        return body
     }
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class ProcessedVideoSNSRequest (
+data class ProcessedVideoSNSRequestBody (
     val Type : String?,
     val MessageId : String?,
     val TopicArn : String?,
