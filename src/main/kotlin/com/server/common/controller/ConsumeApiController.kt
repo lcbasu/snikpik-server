096bb7428576a1d71f3787f5c9d8ff36937dee4c
Lokesh Basu <lokesh.basu@gmail.com>
2021-11-14 11:49:29 +0530

    fix: Read request

--- src/main/kotlin/com/server/common/controller/ConsumeApiController.kt
+++ src/main/kotlin/com/server/common/controller/ConsumeApiController.kt
@@ -1,5 +1,6 @@
 package com.server.common.controller
 
+import com.amazonaws.auth.PropertiesCredentials
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -11,10 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
+import java.io.*
+import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -31,47 +30,65 @@ class ConsumeApiController {
     @RequestMapping(value = ["/awsVideoProcessingCompleted"], method = [RequestMethod.POST])
     @ResponseBody
     fun awsVideoProcessingCompleted(request: HttpServletRequest, response: HttpServletResponse){
-        val body = getBody(request)
-        logger.info("body: $body")
-        val bodyObject = try {
-            jacksonObjectMapper().readValue(body, ProcessedVideoSNSRequestBody::class.java)
-        } catch (e: Exception) {
-            null
+
+
+        try {
+            // Scan request into a string
+            val scanner = Scanner(request.inputStream)
+            val builder = StringBuilder()
+            while (scanner.hasNextLine()) {
+                builder.append(scanner.nextLine())
+            }
+
+            logger.info("builder: ${builder.toString()}")
+
+            // Parse the JSON message
+            val stream: InputStream = ByteArrayInputStream(builder.toString().toByteArray())
+            val message = jacksonObjectMapper().readValue(stream, MutableMap::class.java)
+
+            logger.info("message: ${message.toString()}")
+
+            // Confirm the subscription
+            if (message["Type"] == "SubscriptionConfirmation") {
+//                val sns = AmazonSNSClient(
+//                    PropertiesCredentials(SnsConfirmerServlet::class.java.getResourceAsStream("AwsCredentials.properties"))
+//                )
+//                sns.confirmSubscription(ConfirmSubscriptionRequest(message["TopicArn"], message["Token"]))
+            }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
         }
-        logger.info("Call mediaHandlerService for InputFile: ${bodyObject?.Message?.InputFile}")
-        mediaHandlerService.startProcessingAfterVideoProcessing(bodyObject?.Message)
+
+
+
+
+//        val body = getBody(request)
+//        logger.info("body: $body")
+//        val bodyObject = try {
+//            jacksonObjectMapper().readValue(body, ProcessedVideoSNSRequestBody::class.java)
+//        } catch (e: Exception) {
+//            null
+//        }
+//        logger.info("Call mediaHandlerService for InputFile: ${bodyObject?.Message?.InputFile}")
+//        mediaHandlerService.startProcessingAfterVideoProcessing(bodyObject?.Message)
     }
 
     @Throws(IOException::class)
     fun getBody(request: HttpServletRequest): String? {
-        var body: String? = null
-        val stringBuilder = StringBuilder()
-        var bufferedReader: BufferedReader? = null
         try {
-            val inputStream: InputStream? = request.inputStream
-            if (inputStream != null) {
-                bufferedReader = BufferedReader(InputStreamReader(inputStream))
-                val charBuffer = CharArray(128)
-                var bytesRead = -1
-                while (bufferedReader.read(charBuffer).also { bytesRead = it } > 0) {
-                    stringBuilder.append(charBuffer, 0, bytesRead)
-                }
-            } else {
-                stringBuilder.append("")
+            val inputStream: InputStream = request.inputStream
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
+            var line: String? = bufferedReader.readLine()
+            while (line != null) {
+                stringBuilder.append(line)
+                line = bufferedReader.readLine()
             }
-        } catch (ex: IOException) {
-            throw ex
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close()
-                } catch (ex: IOException) {
-                    throw ex
-                }
-            }
+            return stringBuilder.toString()
+        } catch (e: IOException) {
+            logger.error("Error while reading request body", e)
+            return null
         }
-        body = stringBuilder.toString()
-        return body
     }
 }
 
