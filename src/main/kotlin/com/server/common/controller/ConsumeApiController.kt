002a0e56c39f66a91868625230caf6e40c2b86ab
Lokesh Basu <lokesh.basu@gmail.com>
2021-11-14 12:35:39 +0530

    fix: Read request

--- src/main/kotlin/com/server/common/controller/ConsumeApiController.kt
+++ src/main/kotlin/com/server/common/controller/ConsumeApiController.kt
@@ -49,6 +49,16 @@ class ConsumeApiController {
             logger.info("message: ${message.toString()}")
             logger.info("messageType: ${message.getOrDefault("Type", "NOT_FOUND")}")
             logger.info("messageMessage: ${message.getOrDefault("Message", "NOT_FOUND")}")
+
+            val bodyObject = try {
+                jacksonObjectMapper().readValue(stream, ProcessedVideoSNSRequestBody::class.java)
+            } catch (e: Exception) {
+                null
+            }
+
+            logger.info("bodyObject: ${bodyObject.toString()}")
+            logger.info("MessageId: ${bodyObject?.MessageId}")
+
         } catch (e: java.lang.Exception) {
             e.printStackTrace()
         }
@@ -69,7 +79,7 @@ data class ProcessedVideoSNSRequestBody (
     val MessageId : String?,
     val TopicArn : String?,
     val Subject : String?,
-    val Message : ProcessedVideoMessage?,
+//    val Message : ProcessedVideoMessage?,
     val Timestamp : String?,
     val SignatureVersion : Int?,
     val Signature : String?,
