eb544f3aa4b81910f07fd4b8254793cb6947a62c
Lokesh Basu <lokesh.basu@gmail.com>
2021-12-11 15:09:22 +0530

    feat: Use apache csv reader

--- src/main/kotlin/com/server/common/provider/CSVDataProvider.kt
+++ src/main/kotlin/com/server/common/provider/CSVDataProvider.kt
@@ -1,26 +1,19 @@
 package com.server.common.provider
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.dataformat.csv.CsvMapper
-import com.fasterxml.jackson.dataformat.csv.CsvSchema
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.server.ud.dto.CityLocationData
 import io.sentry.Sentry
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVRecord
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.io.File
 
+
 @Component
 class CSVDataProvider {
 
     private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
-
-    val schema = CsvSchema.emptySchema().withHeader()
-    val mapper = CsvMapper().apply {
-        registerKotlinModule()
-        propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
-    }
 
 //    final val citiesLocationData: Map<String, CityLocationData>
 
@@ -28,28 +21,44 @@ class CSVDataProvider {
 //        citiesLocationData = loadCitiesLocationData()
 //    }
 
+    fun getRowsFromCSVFile(fileName: String): List<CSVRecord> {
+        return try {
+            val reader = File::class.java.getResourceAsStream("/$fileName").bufferedReader()
+            CSVFormat.DEFAULT.parse(reader).records
+        } catch (e: Exception) {
+            logger.error("Error while reading CSV data for file: $fileName.")
+            e.printStackTrace()
+            Sentry.captureException(e)
+            emptyList()
+        }
+    }
+
     fun loadCitiesLocationData(): Map<String, CityLocationData> {
         return try {
+            val rows = getRowsFromCSVFile("cities_location.csv")
             val result = mutableMapOf<String, CityLocationData>()
-            val reader = mapper.readerFor(CityLocationData::class.java)
-            reader
-                .with(schema)
-                .readValues<CityLocationData>(
-                    File::class.java.getResourceAsStream("/cities_location.csv")
-                )
-                .readAll()
+            rows
                 .map {
-                    if (result.containsKey(it.city)) {
+                    val rowData = CityLocationData(
+                    city = it.get(0),
+                    state = it.get(1),
+                    zipcode = it.get(2),
+                    country = it.get(3),
+                    countryCode = it.get(4),
+                    latitude = 0.0,
+                    longitude = 0.0,
+                    )
+                    if (result.containsKey(rowData.city)) {
                         // Same city is appearing twice
                         // So check if the state is different
                         // and then store it with state name
-                        val oldValue = result.getValue(it.city)
-                        if (oldValue.state != it.state) {
-                            val cityName = "${it.city} - (${it.state})"
-                            result[cityName] = it.copy(city = cityName)
+                        val oldValue = result.getValue(rowData.city)
+                        if (oldValue.state != rowData.state) {
+                            val cityName = "${rowData.city} - (${rowData.state})"
+                            result[cityName] = rowData.copy(city = cityName)
                         }
                     } else {
-                        result[it.city] = it
+                        result[rowData.city] = rowData
                     }
                 }
             result.toSortedMap()
